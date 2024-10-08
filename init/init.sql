6650ea77cea4418c3f8269fb21663119f763749a
Miutaku <miutaku@miutakuMacBook-Pro.local>
2024-10-09 04:39:02 +0900

    DATETIMEにした

--- init/init.sql
+++ init/init.sql
@@ -4,15 +4,15 @@ CREATE TABLE tickets (
   ticketID BIGINT AUTO_INCREMENT PRIMARY KEY,
   ticketService VARCHAR(100),
   eventName VARCHAR(100),
-  eventDate TIMESTAMP,
+  eventDate DATETIME,
   eventPlace VARCHAR(100)
 );
 
 CREATE TABLE user_details (
   userID VARCHAR(100),
-  ticketRegistDate TIMESTAMP,
+  ticketRegistDate DATETIME,
   ticketCount INT,
   isReserve BOOLEAN,
-  payLimitDate TIMESTAMP,
+  payLimitDate DATETIME,
   PRIMARY KEY (userID, ticketRegistDate)
 );
