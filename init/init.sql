846c8840a3f0699de6113b3a8621f40136917088
Miutaku <miutaku@miutakuMacBook-Pro.local>
2024-10-09 04:35:59 +0900

    TIMESTAMPにしたほうがいいかなぁ

--- init/init.sql
+++ init/init.sql
@@ -4,14 +4,15 @@ CREATE TABLE tickets (
   ticketID BIGINT AUTO_INCREMENT PRIMARY KEY,
   ticketService VARCHAR(100),
   eventName VARCHAR(100),
-  eventDate DATE,
+  eventDate TIMESTAMP,
   eventPlace VARCHAR(100)
 );
 
 CREATE TABLE user_details (
-  userID VARCHAR(100) PRIMARY KEY,
-  ticketRegistDate DATETIME,
+  userID VARCHAR(100),
+  ticketRegistDate TIMESTAMP,
   ticketCount INT,
   isReserve BOOLEAN,
-  payLimitDate DATE
+  payLimitDate TIMESTAMP,
+  PRIMARY KEY (userID, ticketRegistDate)
 );
